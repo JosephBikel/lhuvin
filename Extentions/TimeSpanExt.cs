ef1f11fa76c6f3e08ae6b566cbf15ede101625ee
JosephBikel <78972799+JosephBikel@users.noreply.github.com>
2021-10-03 13:22:49 -0400

    Add files via upload

---
+++ Extentions/TimeSpanExt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lhuvin.Extentions
+{
+    public static class TimeSpanExt
+    {
+        public static string ToTimeString(this TimeSpan time)
+        {
+            var timeString = "";
+
+            if (time.Hours > 0)
+            {
+                timeString += time.Hours.ToString().TrimStart('0') + " שעה";
+               
+            }
+            if (time.Minutes > 0)
+            {
+                timeString += " " + time.Minutes.ToString().TrimStart('0') + " מינוט";
+            }
+            return timeString;
+        }
+
+        public static TimeSpan Sum(this IEnumerable<TimeSpan> times)
+        {
+            var timeSum = new TimeSpan(times.Select(t => t.Hours).Sum(),
+                           times.Select(t => t.Minutes).Sum(),
+                            times.Select(t => t.Seconds).Sum());
+            return timeSum;
+        }
+    }
+}
